a1d111eb85f4412e9335481047912f47571c3279
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2023-04-02 02:00:55 +0700

    Update see movie list integration test

--- spec/integration/see_movie_list_spec.rb
+++ spec/integration/see_movie_list_spec.rb
@@ -2,38 +2,35 @@
 
 require 'rails_helper'
 
-RSpec.describe 'See Movie list', type: :feature do
-  let(:user) { create(:user) }
-  let!(:movies) { create_list(:movie, 3, user:) }
+RSpec.describe 'See Movie List', type: :feature do
+  let!(:users) { create_list(:user, 3) }
+  let(:movie) { build(:movie) }
+  let(:youtube_url) { 'https://www.youtube.com/watch?v=AbCdEfGhIjK' }
+  let(:persisted_movie) { Movie.last }
+  let(:embed_youtube_movie) { "https://www.youtube.com/embed/#{persisted_movie.youtube_id}" }
 
   before do
+    users.each do |user|
+      sign_in user
+      visit new_movie_path
+      fill_in 'Title', with: movie.title
+      fill_in 'Description', with: movie.description
+      fill_in 'Youtube url', with: youtube_url
+      click_on 'Share'
+      sign_out user
+    end
     visit root_path
   end
 
-  context 'when a guest user wants to see the movie list' do
-    it 'displays a list of shared movies' do
-      movies.each do |movie|
-        expect_movie_to_be_displayed(movie)
-      end
+  it 'shows shared movies by users' do
+    Movie.all.each do |movie|
+      expect_to_see_movie(movie)
     end
   end
 
-  context 'when a registered user wants to see the movie list' do
-    before do
-      sign_in user
-      visit root_path
-    end
-
-    it 'displays a list of shared movies' do
-      movies.each do |movie|
-        expect_movie_to_be_displayed(movie)
-      end
-    end
-  end
-
-  def expect_movie_to_be_displayed(movie)
+  def expect_to_see_movie(movie)
     expect(page).to have_content(movie.title)
     expect(page).to have_content(movie.description)
-    expect(page).to have_css("iframe[src*='#{movie.youtube_id}']")
+    expect(page).to have_css("iframe[src*='https://www.youtube.com/embed/#{movie.youtube_id}']")
   end
 end
